1bffe3fbf4bbd07f97be4e48db20d07e68294c9a
Warryz <8317771+Warryz@users.noreply.github.com>
2020-03-02 20:55:57 +0100

    Added more comments to the deploy script.

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-# hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# Really important: If you use a theme add quotation marks nasty errors when creating your pages.
 ./hugo -t "hugo-coder"
 
 # Go To Public folder
@@ -15,12 +15,16 @@ cd public
 # Add changes to git.
 git add .
 
+# Read a commit message in
+read -p "Please enter a commit message: " msg
+
 # Commit changes.
-msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# Finally: push source and build repos.
 git push origin master
+
+# From here on your changes to github pages are live so that you can view them.
